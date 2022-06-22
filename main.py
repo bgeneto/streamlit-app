c0c90fc2ab01cf9ac67f2a6c913c4c70bae5ae70
Bernhard Enders <bgeneto@users.noreply.github.com>
2022-06-22 14:06:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -62,98 +62,21 @@ def st_layout(title: str = "Streamlit App") -> None:
         layout="centered")
     st.title(title)
 
-
-class Experiment:
-    def __init__(self, sensors: int, pps: int) -> None:
-        # number of sensors
-        self.sensors = sensors
-        # read points per sensor
-        self.pps = pps
-        self.pts = self.sensors * self.pps
-
-
+    
 def initial_sidebar_config():
     # sidebar contents
     sidebar = st.sidebar
     sidebar.subheader("..:: MENU ::..")
 
-
-def reshapedf(ini: int, df: pd.DataFrame) -> None:
-    # crop the beginning of data
-    df = df[ini:]
-
-    # crop the end of data
-    nlen = len(df) - len(df) % exp.pts
-    df = df[:nlen]
-
-    # reshape dataframe
-    df = pd.DataFrame((df.values.reshape(exp.pts, -1)))
-
-    # print all data  
-    #st.write(df)
-
-    # split data by sensors
-    split_sensors(df)
-
-
-def split_sensors(df: pd.DataFrame) -> None:
-    # split sensors data in the middle point (not average)
-    ndf = pd.DataFrame()
-    pos = 10; i = 0
-    while pos < len(df):
-        i += 1
-        sr = df.iloc[pos-1].rename(f"sensor {i}")
-        ndf = pd.concat([ndf, sr], axis=1)
-        pos = pos + exp.pps
-
-    st.write(ndf)
-    # plot sensors 
-    fig = px.line(ndf)
-    st.plotly_chart(fig, use_container_width=True)
-
-
+    
 def main():
     # record start time
     start = timer()
 
     # config sidebar
-    #sidebar = initial_sidebar_config()
+    sidebar = initial_sidebar_config()
 
-    # upload and read csv file
-    st.write("## 1. Upload your CSV file")
-    uploaded_file = st.file_uploader(
-        "Choose a CSV file below:", 
-        type=['csv'],
-        accept_multiple_files=False)
-
-    df = None
-    # check if file was uploaded
-    if uploaded_file is not None:
-        try:
-            df = pd.read_csv(uploaded_file, delimiter=',',
-                             skiprows=1, usecols=[1])
-            st.success(
-                ":white_check_mark: File uploaded and read with success!")
-        except Exception as e:
-            st.exception(e)
-            stop()
-
-        # plot whole file
-        fig = px.line(df)
-        st.plotly_chart(fig, use_container_width=True)
-
-        # choose the initial point
-        st.write("## 2. Initial point")
-        help_txt = "Tip: zoom in the graphic above to better " \
-                   "choose your initial point."
-        ini = 0
-        ini = st.number_input('Choose the initial (index) point by inspecting the figure above', format="%d", min_value=0,
-                              help=help_txt,
-                              on_change=reshapedf,
-                              args=(ini, df,),
-                              key=ini)
-
-        reshapedf(ini, df)
+    st.write("# Hello World!")
 
     # copyright, version and running time info
     end = timer()
@@ -166,10 +89,7 @@ if __name__ == '__main__':
     force_streamlit = True
 
     # page title/header
-    title = "Experiment :pencil: Prof. Mauro"
-
-    # experiment data
-    exp = Experiment(sensors=8, pps=20)
+    title = "Page Title Here"
 
     # increase pandas default output precision from 6 decimal places to 7
     pd.set_option("display.precision", 7)
